38eef2507eaed081952f752d6cf4f0d9a6666de3
cachekai <cachedb@163.com>
2017-03-08 13:53:05 +0800

    Create diskCheck.sh
    A script to check disk usage of servers among the same net

---
+++ diskCheck.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#Filename: disklog.sh
+#Description: Monitor disk usage health for remote systems, substitute the IP_LIST with your IPs
+
+logfile="diskusage.log"
+
+if [[ -n $1 ]]; then 
+	logfile=$1
+fi 
+
+if [ ! -e $logfile ]; then 
+	printf "%-8s %-14s %-9s %-8s %-6s %-6s %-6s %s\n" "Date" "IP address" "Device" "Capacity" "Used" "Free" "Percent" "Status" > $logfile
+fi 
+
+IP_LIST="x.x.x.x x.x.x.x"
+
+(for ip in $IP_LIST; do
+		ssh root@$ip 'df -h' | grep ^/dev/ > /tmp/$$.df
+
+		while read line;
+		do
+			cur_date=$(date +%D)
+			printf "%-8s %-14s " $cur_date $ip
+			echo $line | awk '{ printf("%-9s %-8s %-6s %-6s %-8s", $1, $2, $3, $4, $5); }'
+			pusg=$(echo $line | egrep -o "[0-9]+%")
+			pusg=${pusg/\%/}
+			if [ $pusg -lt 80 ]; then
+				echo SAFE
+			else
+				echo ALERT
+			fi
+
+		done< /tmp/$$.df
+
+done) >> $logfile
